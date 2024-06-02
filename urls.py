bb1655f2004294b9955a4a6ade4e69fa1209e7eb
sudheer_25 <101689053+sudheer-9496@users.noreply.github.com>
2024-06-02 18:15:58 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,12 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('index', views.index, name='index'),
-    path('register', views.register, name='register'),
-    path('', views.login, name='login'),
-    path('garden/', views.garden_view, name='garden'),
-       path('furniture/', views.furniture_view, name='furniture'),
-          path('fruitslist/', views.fruits_list, name='fruitslist'),
-         path('payment/', views.payment_list, name='payment'),
+    path('admin/', admin.site.urls),
+    path('', include('ecomart.urls')),
 ]
